3017302346c04b7517c7d2faed01d16721dcbe08
richardbrash <richard.brash@richardbrashmarketing.com>
2015-11-13 14:43:58 -0500

    Add community engagement sign up page

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
 var express = require('express');
+var Poet = require('poet');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var blog = require('./routes/blog');
+//var users = require('./routes/users');
 var signup = require('./routes/signup');
 
 var app = express();
+
+var poet = Poet(app, {
+    postsPerPage: 5,
+    posts: './_posts',
+    metaFormat: 'json',
+    routes: {
+        '/blog/posts/:post': 'post',
+        '/blog/pagination/:page': 'page',
+        '/blog/tags/:tag': 'tag',
+        '/blog/categories/:category': 'category'
+    }
+});
+
+
+poet.init().then(function () {
+    // ready to go!
+});
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,11 +41,12 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
-app.use('/signup', signup);
+app.use('/blog', blog);
+//app.use('/users', users);
+//app.use('/signup', signup);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
