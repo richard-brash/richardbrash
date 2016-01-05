90d66e0adc8ab5e3310e6172c6dad8325ac8cde0
richardbrash <richard.brash@richardbrashmarketing.com>
2016-01-05 09:39:41 -0500

    Adding some logging to figure out what the hell is going on

--- routes/blog.js
+++ routes/blog.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res) { res.render('index'); });
+router.get('/', function (req, res) {
+    console.log("*** Here we are in the blog route ***");
+    res.render('index');
+
+});
 
 module.exports = router;
